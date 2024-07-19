01af28526123880df90e9a98e4dadc8bf829ba45
Elena <elena.regiionh@gmail.com>
2024-07-19 18:02:00 +0300

    delete sleep

--- src/test/java/LanguageChangeCheckLinguiseTest.java
+++ src/test/java/LanguageChangeCheckLinguiseTest.java
@@ -16,8 +16,6 @@ public class LanguageChangeCheckLinguiseTest extends BaseLinguise {
 
         open("https://www.linguise.com/");
 
-        sleep(3000);
-
         $(".linguise_flags.linguise_language_icon").click();
 
         $$(".linguise_lang_name").filter(text(language)).get(0).click();
@@ -32,11 +30,7 @@ public class LanguageChangeCheckLinguiseTest extends BaseLinguise {
 
         open("https://www.linguise.com/");
 
-        sleep(3000);
-
         $(".linguise_flags.linguise_language_icon").click();
-        
-        sleep(3000);
 
         $$(".linguise_lang_name").filter(text(chooseLang.name())).get(0).click();
 
